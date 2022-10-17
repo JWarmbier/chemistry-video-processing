5b7fdb92d596b3937ae327669042cd26259d790e
Jaroslaw Warmbier <warmbier.jaroslaw@gmail.com>
2022-10-17 21:49:07 +0200

    Add printed logs

--- video_processing.py
+++ video_processing.py
@@ -1,6 +1,10 @@
 import cv2 as cv
 from pathlib import Path
+from progress_bar import printProgressBar
 import os
+
+MS_IN_SEC = 1000        # 1000 ms/sec
+
 
 class VideoProcessing:
     def set_video_path(self, path):
@@ -10,10 +14,17 @@ class VideoProcessing:
         self.path = path
         self.video_name = path.stem
 
-    def save_snapshots(self, output_dir, interval=1):
+    def get_video_duration(self, video):
+        fps = fps = video.get(cv.CAP_PROP_FPS)
+        frame_count = int(video.get(cv.CAP_PROP_FRAME_COUNT))
+        duration = frame_count/fps
+
+        return duration
+
+    def save_snapshots(self, output_dir, interval_sec=1):
         video = cv.VideoCapture(os.fspath(self.path))
         output_path = Path(output_dir).joinpath(self.video_name)
-        interval_ms = int(interval*1000)
+        interval_ms = int(interval_sec*MS_IN_SEC)
         frame_counter = 0
         success = True
 
@@ -22,6 +33,10 @@ class VideoProcessing:
 
         while success:
             success, frame = video.read()
+
+            progress_status = min(frame_counter*interval_ms/1000, self.get_video_duration(video))
+            printProgressBar(progress_status, self.get_video_duration(video))
+
             if success:
                 img_path = str(output_path.joinpath(f'{int(interval_ms*frame_counter)}_ms.jpg'))
                 cv.imwrite(img_path, frame)
